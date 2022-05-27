5602f196f16292b08a6ab3f9d4385d9479e837fc
Diego López <diegolopezselva@gmail.com>
2022-05-27 12:06:35 +0200

    Adding more changes to the class herramientaController and changed to the herramientas

--- herramienta/herramienta.controller.js
+++ herramienta/herramienta.controller.js
@@ -50,26 +50,19 @@ function _delete(req, res, next) {
 
 function createSchema(req, res, next) {
     const schema = Joi.object({
-        title: Joi.string().required(),
-        firstName: Joi.string().required(),
-        lastName: Joi.string().required(),
-        role: Joi.string().valid(Role.Admin, Role.Herramienta).required(),
-        email: Joi.string().email().required(),
-        password: Joi.string().min(6).required(),
-        confirmPassword: Joi.string().valid(Joi.ref('password')).required()
+        ID: Joi.int().required(),
+        Tipo: Joi.ENUM('Hardware', 'Software').required(),
+        Nombre: Joi.string().required(),
     });
     validateRequest(req, next, schema);
 }
 
 function updateSchema(req, res, next) {
     const schema = Joi.object({
-        title: Joi.string().empty(''),
-        firstName: Joi.string().empty(''),
-        lastName: Joi.string().empty(''),
-        role: Joi.string().valid(Role.Admin, Role.Herramienta).empty(''),
-        email: Joi.string().email().empty(''),
-        password: Joi.string().min(6).empty(''),
-        confirmPassword: Joi.string().valid(Joi.ref('password')).empty('')
-    }).with('password', 'confirmPassword');
+        
+        ID: Joi.int().empty(''),
+        Tipo: Joi.ENUM('Hardware', 'Software').empty(''),
+        Nombre: Joi.string().empty(''),
+    });
     validateRequest(req, next, schema);
 }
