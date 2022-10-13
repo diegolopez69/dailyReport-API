3a42c82b0bfc1bb75dcf2d44c3c7e0eef93c5897
Diego López <diegolopezselva@gmail.com>
2022-10-13 16:27:53 +0200

    Update the route of get all users

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -1,15 +1,15 @@
 const { verifySignUp } = require("../middleware");
 const Auth = require("../controllers/auth.controller");
 
-module.exports = function(app) {
-  app.use(function(req, res, next) {
+module.exports = function (app) {
+  app.use(function (req, res, next) {
     res.header(
       "Access-Control-Allow-Headers",
       "x-access-token, Origin, Content-Type, Accept"
     );
     next();
   });
-  
+
   app.post(
     "/api/auth/signup",
     [
@@ -19,14 +19,12 @@ module.exports = function(app) {
     Auth.signup
   );
 
-  app.get(
-    "/api/auth/signup",
-    Auth.findAll
-  );
+  // Retrieve all users
+  app.get("/api/user",Auth.findAll);
 
   // Retrieve a single User with id
   app.get("/api/user/:id", Auth.findOne);
-  
+
   app.post("/api/auth/signin", Auth.signin);
 
   // Update a User with id
