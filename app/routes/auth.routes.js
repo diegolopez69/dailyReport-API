91385befeeb78346e3fdbeb339fae539b0c6cebb
Diego López <diegolopezselva@gmail.com>
2022-11-02 13:00:24 +0100

    Now the routes of users has the protection of admin

--- app/routes/auth.routes.js
+++ app/routes/auth.routes.js
@@ -1,8 +1,11 @@
 const { verifySignUp } = require("../middleware");
+const { authJwt } = require("../middleware");
 const Auth = require("../controllers/auth.controller");
 const UserRol = require("../controllers/user_roles.controller");
 
 module.exports = function (app) {
+
+  // Sign in on the application
   app.use(function (req, res, next) {
     res.header(
       "Access-Control-Allow-Headers",
@@ -19,22 +22,23 @@ module.exports = function (app) {
     ],
     Auth.signup
   );
- 
-  // Retrieve all users
-  app.get("/api/user", Auth.findAll);
-
-  // Retrieve all users with roles
-  app.get("/api/user-roles", UserRol.findAll);
-
-  // Retrieve a single User with id
-  app.get("/api/user/:id", Auth.findOne);
 
   // Login on the application
   app.post("/api/auth/signin", Auth.signin);
 
+  // Retrieve all users
+  app.get("/api/user", [authJwt.verifyToken, authJwt.isAdmin], Auth.findAll);
+
+  // Retrieve all users with roles
+  app.get("/api/user-roles", [authJwt.verifyToken, authJwt.isAdmin], UserRol.findAll);
+
+  // Retrieve a single User with id
+  app.get("/api/user/:id", [authJwt.verifyToken, authJwt.isAdmin], Auth.findOne);
+
   // Update a User with id
-  app.put("/api/user/:id", Auth.update);
+  app.put("/api/user/:id", [authJwt.verifyToken, authJwt.isAdmin], Auth.update);
 
   // Delete a User with id
-  app.delete("/api/user/:id", Auth.delete);
+  app.delete("/api/user/:id", [authJwt.verifyToken, authJwt.isAdmin], Auth.delete);
+
 };  
