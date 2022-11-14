dbc772d6b9fcd809dcb87eca1b1de9a92ad3c34b
Diego Lopez <diegolopezselva@gmail.com>
2022-11-14 16:58:22 +0100

    Adding the relation of chromebook

--- app/models/index.js
+++ app/models/index.js
@@ -1,32 +1,41 @@
-'use strict';
+"use strict";
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
+const fs = require("fs");
+const path = require("path");
+const Sequelize = require("sequelize");
+const process = require("process");
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require('../../config/config.json')[env];
+const env = process.env.NODE_ENV || "development";
+const config = require("../../config/config.json")[env];
 const db = {};
 
 let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  );
 }
 
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+fs.readdirSync(__dirname)
+  .filter((file) => {
+    return (
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+    );
   })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
+  .forEach((file) => {
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    );
     db[model.name] = model;
   });
 
-Object.keys(db).forEach(modelName => {
+Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
     db[modelName].associate(db);
   }
@@ -38,30 +47,32 @@ db.sequelize = sequelize;
 db.tb_computers = require("./ordenador.model.js")(sequelize, Sequelize);
 db.tb_classrooms = require("./aula.model.js")(sequelize, Sequelize);
 db.tb_tools = require("./herramientas.model.js")(sequelize, Sequelize);
-db.tb_aula_herramienta_ordenador = require("./aho.model.js")(sequelize, Sequelize);
+db.tb_aula_herramienta_ordenador = require("./aho.model.js")(sequelize,Sequelize);
 db.user = require("./user.model.js")(sequelize, Sequelize);
 db.role = require("./role.model.js")(sequelize, Sequelize);
 db.tb_checkups = require("./checkups.model.js")(sequelize, Sequelize);
 db.tb_chromebook = require("./chromebook.model.js")(sequelize, Sequelize);
 db.tb_user_roles = require("./user_roles.model.js")(sequelize, Sequelize);
 
-
 db.role.belongsToMany(db.user, {
   through: "tb_user_roles",
   foreignKey: "roleId",
-  otherKey: "userId"
+  otherKey: "userId",
 });
 
 db.user.belongsToMany(db.role, {
   through: "tb_user_roles",
   foreignKey: "userId",
-  otherKey: "roleId"
+  otherKey: "roleId",
 });
 
+ db.tb_classrooms.hasMany(db.tb_chromebook);
+// db.tb_chromebook.belongsTo(db.tb_classrooms, {
+//   foreignKey: "Aula_id",
+// });
+
 db.ROLES = ["user", "admin", "moderator"];
-db.tb_aula_herramienta_ordenador.associate(db)
-db.tb_checkups.associate(db)
-
-
+db.tb_aula_herramienta_ordenador.associate(db);
+db.tb_checkups.associate(db);
 
 module.exports = db;
